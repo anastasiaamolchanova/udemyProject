9f1a83bcd7d22540a908185034b987648024c5da
Anastasiia <kalinovskaya.anastasia@gmail.com>
2019-11-30 16:18:28 +0200

    updated daily budget detection, new functions added

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
 'use strict';
+let moneyData, timeData;
 
-let moneyData = prompt("Ващ бюджет на месяц?");
-let timeData = prompt("Введите дату YYYY-MM-DD");
+function start() {
+    moneyData = prompt("Ваш бюджет на місяць?");
+    timeData = prompt("Введіть дату YYYY-MM-DD");
 
+    while (isNaN(moneyData) || moneyData == "" || moneyData == null) {
+        moneyData = +prompt("Ваш бюджет на місяць?", '');
+    }
+}
 
+start();
 
 let appData = {
     budget: moneyData,
     timeData: timeData,
     expenses: {},
     optionalExpenses: {},
-    income: [],
-    savings: false,
-
-
+    monthIncome: [],
+    savings: true,
 };
 
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введіть обов/'язкову статтю витрат");
+        let b = prompt("У скільки обійдеться?");
 
+        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+            a != '' && b != '' & a.length < 50) {
+            console.log('done');
+            appData.expenses[a] = b;
+            appData.budget -= b;
+        } else {
+            console.log('bad result');
+            i--;
+        }
+    }
+}
+chooseExpenses();
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце");
-    let b = prompt("Во сколько обойдется? ?");
+function detectDayBudget() {
+    alert((appData.budget / 30).toFixed(1));
+}
 
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
-        a != '' && b != '' & a.length < 50) {
-        console.log('done');
-        appData.expenses[a] = b;
-        appData.budget -= b;
-    } else {
-        break;
+detectDayBudget();
+
+function chooseOptExpenses() {
+    let a = prompt('Стаття необов\'язкових витрат?');
+    appData.optionalExpenses[1] = a;
+
+}
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('Яка сума накопичувань?'),
+            percent = +prompt('Який відсоток?');
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert("Дохід в місяць з Вашого депозиту: " + appData.monthIncome);
     }
 }
 
-let moneyPerDay = alert(appData.budget / 30);
+checkSavings();
